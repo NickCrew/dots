9df8be46a86601afcc44c61a60cf5c63ea42ea4f
Nick Ferguson <nick.ferguson@threatx.com>
2022-01-24 12:13:56 -0500

    create bootstrap scripts

--- .local/share/dots/include/10-install-pkgs-lua.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-#
-
-brew install luarocks
-luarocks install --server=https://luarocks.org/dev luaformatter
-luarocks install fennel
--- .local/share/dots/include/50-setup-gh.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-#
-
-brew install gh
-gh extension install gennaro-tedesco/gh-f
