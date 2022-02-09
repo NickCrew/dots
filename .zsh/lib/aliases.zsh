dc7e5ac5498f98731c68edfb42d8797d06613a59
Nick Ferguson <ncf@nickferguson.dev>
2022-02-09 18:14:10 -0500

    add some zsh plugins

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -8,3 +8,16 @@ alias tower='gittower'
 alias lynx='lynx -vikeys'
 alias lzg='lazygit'
 alias lzd='lazydocker'
+
+# Remove any existing TMUX aliases and set our own
+tx_aliases=$(alias | grep tmux)
+for a in $tx_aliases; do
+	unalias $(echo $a | cut -d '=' -f 1)
+done
+alias txa='tmux attach -t'
+alias txs='tmux new-session -s'
+alias txl='tmux list-sessions'
+alias txk='tmux kill-server'
+alias tkss='tmux kill-session -t'
+
+
