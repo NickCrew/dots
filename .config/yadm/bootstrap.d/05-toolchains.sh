3cc76e99036e830810e6632c2f6d241381d92e42
Nick Ferguson <ncf@nickferguson.dev>
2023-09-26 03:06:16 -0400

    simpler better

--- .config/yadm/bootstrap.d/05-toolchains.sh
+++ .config/yadm/bootstrap.d/05-toolchains.sh
@@ -2,10 +2,33 @@
 
 OS=$(uname -s)
 PYVERSION=${PYVERSION:-3.11.4}
+PREFIX="${TMUX_PREFIX:-$HOME/.local}"
+
+TMUX_REMOTE="https://github.com/tmux/tmux"
+TPM_REMOTE="https://github.com/tmux-plugins/tpm"
+TMUX_PLUG="${TMUX_PLUG:-$HOME/.tmux/plugins}"
+
+
+install_tmux() {
+	TMUX_SRC="${PREFIX}/src/tmux"
+	mkdir -p "${TMUX_SRC}" "${PREFIX}/share/man/man1" \
+		&& cd "${TMUX_SRC}"
+
+	if [ -d .git ]; then git pull -f;
+	else git clone ${TMUX_REMOTE}; fi
+
+	sh autogen.sh 
+	./configure --enable-utf8proc --prefix=${PREFIX}
+	make && make install 
+
+	[ ! -d "${TMUX_PLUG}/tpm" ] \
+      && git clone ${TPM_REMOTE} "${TMUX_PLUG}"
+}
+
 
 # Using Ansible for Docker Registry Load Testing
 #
-function install_toolchain_brew() {
+function install_brew() {
   if [ "$OS" = "Darwin" ]; then
       xcode-select --install
       # install homebrew if it's missing
@@ -23,12 +46,16 @@ function install_brew_packages() {
     autoconf \
     automake \
     aws-iam-authenticator \
+    bison \
+    bzip2 \
     jq \ 
     k9s \
     kube-linter \ 
     kubectl \ 
     kubectx \
     luarocks \
+	libevent \
+	ncurses \
     nvm \
     pipx \ 
     pkg-config
@@ -36,14 +63,16 @@ function install_brew_packages() {
     pyenv-virtualenv \
     pyenv-virtualenvwrapper \
     tree-sitter \
-    yadm 
-
+	utf8proc 
 }
 
-function install_toolchain_rust () {
+function install_rust () {
   if ! command -v cargo >/dev/null; then
       curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   fi
+}
+
+function install_cargo_packages() {
   source ~/.cargo/env
   cargo install \
     broot \
@@ -54,7 +83,7 @@ function install_toolchain_rust () {
     zoxide 
 }
 
-function install_toolchain_node() {
+function install_node() {
   if (( ! $+commands[node] )); then
     if (( ! $+commands[nvm] )); then
 		PROFILE=/dev/null curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
@@ -63,7 +92,7 @@ function install_toolchain_node() {
   fi
 }
 
-function install_toolchain_python() {
+function install_python() {
   # Return immediately if poetry is not found
   if (( $+commands[pyenv] )); then
 
@@ -78,12 +107,53 @@ function install_toolchain_python() {
   fi
 }
 
+function install_python_cli_packages() {
+  pipx install \
+    nvr \
+    ipython \
+    cfn-lint \
+    pygmentize \
+    httpie \
+    pyfunky \
+}
+
 # Install Neovim
-function install_neovim() {
+function install_npm_global_packages() {
   git clone git@github.com:NickCrew/nvim-pde.git 
   ~/.config/nvim/tools/update-nvim-nightly
-  npm install -g neovim
+  npm install -g \
+    neovim \
+    typescript
 }
 
 
+## Main ##
 
+# Homebrew
+if [[ ! -z `echo $YADM_BOOTSTRAP_SKIP | grep brew` ]]; then
+  install_brew
+  install_brew_packages
+fi
+
+# Rust
+if [[ ! -z `echo $YADM_BOOTSTRAP_SKIP | grep rust` ]]; then
+  install_rust
+  install_cargo_packages
+fi
+
+# Node
+if [[ ! -z `echo $YADM_BOOTSTRAP_SKIP | grep node` ]]; then
+  install_node
+  install_npm_global_packages
+fi
+
+# Python
+if [[ ! -z `echo $YADM_BOOTSTRAP_SKIP | grep python` ]]; then
+  install_python
+  install_python_cli_packages
+fi
+
+# Tmux
+if [[ ! -z `echo $YADM_BOOTSTRAP_SKIP | grep tmux` ]]; then
+  install_tmux
+fi
